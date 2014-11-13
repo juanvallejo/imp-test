51bda22b67d10778727c02bdfb6b3fa91e9cb662
juanvallejo <juan.vallejo.12@cnu.edu>
2014-11-12 23:10:26 -0500

    add web interface, add comments

--- imp.js
+++ imp.js
@@ -1,18 +1,43 @@
-
-var fs 		= require('fs');
-var http 	= require('http');
-var https 	= require('https');
+/**
+* Provided under the MIT License (c) 2014
+* See LICENSE @file for details.
+*
+* @file scanner.js
+*
+* @author juanvallejo
+* @date 10/15/14
+*
+* Scanner application 'server'. Handles all data processing and i/o.
+* Reads data from a local mysql database, builds an internal structure
+* with it, and allows for easy manipulation of it. Outputs to .xlsx file.
+*
+* Note: @callback_params are parameters passed to a callback function
+*
+* Important: Requires the following dependencies / node.js packages:
+*
+*		- csv 	-> npm install fast-csv
+* 		- excel -> npm install excel
+* 		- mysql	-> npm install mysql
+* 		- xlsx 	-> npm install xlsx-writer
+*/
 
 /**
- * define stdin variables and settings used in the command line
- * interface part of the program, as well as global flags and 
- * varying settings used in the general application.
+ * import required node packages
+ */
+
+var fs 				= require('fs');
+var http 			= require('http');
+var https 			= require('https');
+var readline		= require('readline');
+
+/**
+ * define variables and settings used by the program's
+ * interface modules. Also contains global flags and 
+ * varying settings used by the server.
 **/
 var useWebInterface = false;										// tells program if accepting user input from web or cli interface
 var server 			= null;											// holds http server object when initialized
 var timeout 		= null;											// holds timeout object; allows it to be cleared when necessary
-var host 			= 'agent.electricimp.com';						// host of server to send response in GET format to
-var stdin 			= process.stdin;								// grabs all keyboard input
 var data 			= '';											// stores chunk data returned from server as response
 var value			= '';											// buffer containing current input entered into command line on enter
 var buffer 			= '';											// buffer containing individual input entered into command line
@@ -20,8 +45,15 @@ var ready 			= false;										// Specifies whether value 'buffer' is ready to b
 																	// used by spreadsheet parser function to indicate contents of file
 																	// have been read and have been added to the database object
 
+/**
+ * define constants used by program interface modules
+ */
+
+WEB_PORT 			= 8000;											// port at which server listens for client connections
+WEB_HOST			= 'agent.electricimp.com';						// host of server to send response in GET format to
+
 // set stdinp to treat all keyboard input as 'raw' input
-stdin.setRawMode(true);
+process.stdin.setRawMode(true);
 
 // set character encoding for keyboard input
 process.stdin.setEncoding('utf8');
@@ -31,7 +63,7 @@ process.stdin.setEncoding('utf8');
  *
  * @event data
 **/
-stdin.on('data', function(key) {
+process.stdin.on('data', function(key) {
 	// if keyboard input is Ctrl-c
 	if(key =='\3') {
 		process.exit();
@@ -46,6 +78,10 @@ stdin.on('data', function(key) {
 				console.log('[CLI] ' + key);
 			}
 		} else {
+			if(!buffer) {
+				return;
+			}
+
 			// parses series of keys before a line break
 			var command = buffer.split('/');
 
@@ -69,12 +105,17 @@ stdin.on('data', function(key) {
 			} else {
 				// command does not exist
 				console.log('[IMP] Command not recognized \'' + command[1] + '\'');
+				// reset value of buffer
+				buffer = '';
 			}
 		}
 	} else {
 		// add current key to char buffer to be parsed as a command
 		buffer += key;
 
+		// if an interface has been chosen, and user is using command line interface, listen for repeated same-key input
+		// if it is the same, reset timeout designed to kill LED in order to keep it on, else, turn LED on. Once no keys are pressed
+		// set timeout takes care of turning LED off
 		if(ready) {
 			if(!useWebInterface) {
 				// if key does not equal the one already pressed, send messag to turn on LED
@@ -84,6 +125,14 @@ stdin.on('data', function(key) {
 
 					// turn on imp LED
 					setImpLEDToOn();
+
+					clearTimeout(timeout);
+
+					// set individual keypress timeout
+					timeout = setTimeout(function() {
+						// turn off LED after a few millisconds of inactivity
+						setImpLEDToOff();
+					}, 720);
 				} else {
 					// detect when key is no longer pressed
 					clearTimeout(timeout);
@@ -105,9 +154,14 @@ stdin.on('data', function(key) {
 
 /**
  * define util functions for program
- *
  */
 
+/**
+ * sends a request to imp agent with param
+ * "?led=1" instructing led to turn on
+ *
+ * @event data
+**/
 function setImpLEDToOn() {
 	console.log('turning on LED');
 	clearTimeout(timeout);
@@ -115,6 +169,12 @@ function setImpLEDToOn() {
 	sendDataToImp(1);
 }
 
+/**
+ * sends a request to imp agent with param
+ * "?led=0" instructing led to turn off
+ *
+ * @event data
+**/
 function setImpLEDToOff() {
 	console.log('turning off LED');
 
@@ -122,36 +182,80 @@ function setImpLEDToOff() {
 	sendDataToImp(0);
 }
 
+/**
+ * inits http function that creates web interface for interacting
+ * with application @ address
+ *
+ * http://localhost:8000
+ *
+ * @event data
+**/
 function initWebServer() {
-	// create an http web server
-	server = http.createServer(function(request, response) {
-		fs.readFile(__dirname + '/index.html', function(err, data) {
+	// create routing for files and serve files
+	function route(path, request, response) {
+		// file extension header definitions
+		var extensions = {
+			'css' : 'text/css',
+			'gif' : 'image/gif',
+			'html' : 'text/html',
+			'ico' : 'image/x-ico',
+			'jpg' : 'image/jpeg',
+			'jpeg' : 'image/jpeg',
+			'json' : 'application/json',
+			'js' : 'application/javascript',
+			'png' : 'image/png',
+			'txt' : 'text/plain'
+		};
+
+		// extract file extension from path
+		var extension = path.split('.');
+		extension = extension[extension.length-1];
+
+		// read file from path
+		fs.readFile(__dirname + path, function(err, file) {
 			if(err) {
-				return console.log('There was an error reading the file \'index.html\' -> ' + err);
+				// advertise error
+				console.log('There was an error reading the file \'' + path + '\' -> ' + err);
+
+				// send error response to client
+				response.writeHead(404);
+				return response.end("File Not Found.");
 			}
 
 			// advertise web interface loaded
-			console.log('[WEB ]web interface loaded for address -> ' + request.connection.remoteAddress);
+			console.log('[WEB] web interface loaded for address -> ' + request.connection.remoteAddress);
 
-			// write headers and send file data
-			response.writeHead({'Content-type' : 'text/html'});
-			response.end(data);
+			response.writeHead(200, {'Content-Type' : extensions[extension]});
+			response.end(file);
 		});
+	};
+
+	// create an http web server
+	server = http.createServer(function(request, response) {
+		//determine if request is a command
+		if(request.url == '/command') {
+			// advertise to console
+			console.log('command detected');
+		} else {
+			// route request to path
+			route((request.url == '/' ? '/index.html' : request.url), request, response);
+		}
 	});
 
-	server.listen(8000);
+	// have server listen on specific port
+	server.listen(WEB_PORT);
 }
 
 /**
- * Sends an http request to imp-board server with a command
+ * Sends an http request to imp-board server with passed command
  * as a GET parameter
  *
- * @param command {String} of 'data' to send to board
+ * @param command {int} defining 'data' to send to board with param of 'led'
 **/
 function sendDataToImp(command) {
 	//send request to imp
 	var request = https.request({
-		host : host,
+		host : WEB_HOST,
 		method : 'GET',
 		path : '/FlA7isVKL8K-?led=' + command
 	}, function(response) {
